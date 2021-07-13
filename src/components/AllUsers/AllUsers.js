e04b7ff408c9bc8766e70e7f3083739e3655deb9
Marc Carlson <marc.carlson30@gmail.com>
2021-07-13 13:30:35 -0500

    working on displaying correct ages

--- src/components/AllUsers/AllUsers.js
+++ src/components/AllUsers/AllUsers.js
@@ -8,9 +8,9 @@ function AllUsers() {
     let [users, setUsers] = useState({});
     let [youngest, setYoungest] = useState(1000);
     let [oldest, setOldest] = useState(0);
+    let [average, setAverage] = useState()
     let [filter, setFilter] = useState("All")
-    let average;
-    // let [currentKey, setCurrentKey] = useState('')
+    let [filteredUsers, setFilteredUsers] = useState(users)
 
     // set value of users after render
     useEffect(() => {
@@ -20,9 +20,8 @@ function AllUsers() {
                     ...cb.val()
                 })
         })
+        // setFilteredUsers(Object.values(users))
     }, [])
-
-    let userArr = Object.values(users)
 
     // find the age of the user and compare to oldest and youngest of 
     // all users
@@ -37,36 +36,56 @@ function AllUsers() {
         return age;
     }
 
-    console.log('age: ' + findAge("1980-08-10"));
-    console.log('users', userArr)
-
-    // for (let i = 0; i < userArr.length; i++) {
-    //     if (findAge(userArr[i].birthday > oldest)) {
-    //         setOldest
-    //     }
-    // }
+    // console.log('age: ' + findAge("1980-08-10"));
+    // console.log('users', filteredUsers)
 
     const handleSelectChange = (e) => {
         let { value } = e.target
         setFilter(value)
+
     }
 
-    let newUserArr;
+    // set ages using filtered user list
+    useEffect(() => {
+        if (filter === "All") {
+            let userList = Object.values(users)
+            console.log('userlist', Object.values(users))
+            // for (let i = 0; userList.length; i++) {
+            //     let age = findAge(userList[i].birthday)
+            //     if (age > oldest) {
+            //         setOldest(age)
+            //     }
+            //     if (age < youngest) {
+            //         setYoungest(age)
+            //     }
+            //     average += age;
+            // }
+            // average /= userList.length
+        }
 
-    if (filter === "All") {
-        newUserArr = userArr
-    } else if (filter === "Male") {
-        newUserArr = userArr.filter(user => user.sex === "Male")
-    } else {
-        newUserArr = userArr.filter(user => user.sex === "Female")
-    }
+    }, [filter])
 
-    console.log(newUserArr)
+
+    // filter users by sex assigned to filteredUsers
+    useEffect(() => {
+        if (filter === "All") {
+            setFilteredUsers(Object.values(users))
+        } else if (filter === "Male") {
+            setFilteredUsers(Object.values(users).filter(user => user.sex === "Male"))
+            console.log('filterdelif', filteredUsers)
+        } else {
+            setFilteredUsers(Object.values(users).filter(user => user.sex === "Female"))
+            // setAges(Object.values(users))
+        }
+    }, [filter, users])
+
+    // console.log('filtered', filteredUsers)
     return (
         <>
             <div className="outter-container">
                 <div className="filter-container">
-                    <select className="form-input" name="sex" value={filter}
+                    <div className="filter-title">Filter</div>
+                    <select className="filter-input" name="sex" value={filter}
                         onChange={handleSelectChange}>
                         <option value="All">All</option>
                         <option value="Male">Male</option>
@@ -75,10 +94,58 @@ function AllUsers() {
                 </div>
                 <div className="age-and-list-container">
                     <div className="age-container">
+                        <div className="youngest-div">
+                            <div className="age-title">
+                                Youngest
+                            </div>
+                            <div className="youngest-age">{youngest}</div>
+                        </div>
+                        <div className="oldest-div">
+                            <div className="age-title">
+                                Oldest
+                            </div>
+                            <div className="oldest-age">{oldest}</div>
+                        </div>
+                        <div className="average-div">
+                            <div className="age-title">
+                                Average
+                            </div>
+                            <div className="average-age">{average}</div>
+                        </div>
 
                     </div>
-                    <div className="user-list-container">
-
+                    <div className="all-user-list-container">
+                        <table className="user-list-table">
+                            <thead className="table-header">
+                                <tr>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Sex</th>
+                                    <th>Birthday</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filter === "ALL" ?
+                                    Object.values(users).map(user => {
+                                        return <tr key={user.firstName}>
+                                            <td>{user.firstName}</td>
+                                            <td>{user.lastName}</td>
+                                            <td>{user.sex}</td>
+                                            <td>{user.birthday}</td>
+                                        </tr>
+                                    })
+                                    :
+                                    Object.values(filteredUsers).map(user => {
+                                        return <tr key={user.firstName}>
+                                            <td>{user.firstName}</td>
+                                            <td>{user.lastName}</td>
+                                            <td>{user.sex}</td>
+                                            <td>{user.birthday}</td>
+                                        </tr>
+                                    })
+                                }
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
